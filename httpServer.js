b42ac33261904aeaba3e85dbe6bbbb1ca4038151
Cory Williams <ucesccw@ucl.ac.uk>
2018-05-05 10:56:17 -0400

    Adding get functionality for web app

--- httpServer.js
+++ httpServer.js
@@ -50,6 +50,33 @@ app.post('/uploadData',function(req,res){
 
 });
 
+app.get('/getQuestions', function (req,res) {
+     pool.connect(function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+        // use the inbuilt geoJSON functionality
+        // and create the required geoJSON format using a query adapted from here:  http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+        // note that query needs to be a single string with no line breaks so built it up bit by bit
+
+        	var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+        	querystring = querystring + "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg.question_location)::json As geometry, ";
+        	querystring = querystring + "row_to_json((SELECT l FROM (SELECT location_name, question, answer_1, answer_2, answer_3, answer_4, answer_correct) As l      )) As properties";
+        	querystring = querystring + "   FROM app_questions  As lg limit 100  ) As f ";
+        	console.log(querystring);
+        	client.query(querystring,function(err,result){
+
+          //call `done()` to release the client back to the pool
+           done(); 
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+});
 
 
 
